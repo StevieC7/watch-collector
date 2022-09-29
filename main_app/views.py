bdb6e2d7d4d7142fb5a6366ea808082b25ad5db0
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-29 14:02:37 -0500

    Init

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+class About(TemplateView):
+    template_name = "about.html"
+class Index(TemplateView):
+    template_name = "index.html"
