48b3232a069bb555164233862c9513ca9543b719
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-30 13:49:09 -0500

    Incremental changes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Watch(models.Model):
+    name = models.CharField(max_length=100)
+    size = models.IntegerField()
+    primary_color = models.Choices(
+        choices=[
+            ("Black", "black"),
+            ("White", "white"),
+            ("Gold", "gold"),
+            ("Silver", "silver"),
+            ("Brown", "brown")
+        ],
+        default="Black"
+    )
+    image = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
